43b0bb407b24fff085129ea202b10c1eecf15e47
Nestertsov Maksym <nestertsovmaxim@gmail.com>
2022-08-22 16:02:05 +0300

    created app

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by M1 on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
